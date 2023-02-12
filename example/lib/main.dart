cdb706bb0eafae6cac4fa584647005929c0da150
Hamza ZOGHMAR <ha.zoghmar@gmail.com>
2023-02-12 14:37:48 +0100

    added openApp and openMarket functions

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,36 +16,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-  final _appActivityLauncherPlugin = AppActivityLauncher();
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await _appActivityLauncherPlugin.getPlatformVersion() ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
+  final _app = AppActivityLauncher();
 
   @override
   Widget build(BuildContext context) {
@@ -53,11 +24,20 @@ class _MyAppState extends State<MyApp> {
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Plugin example app'),
+          centerTitle: true,
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: ElevatedButton(
+            onPressed: _openVibePlyr,
+            child: const Text("Open VibePlyr"),
+          ),
         ),
       ),
     );
+  }
+
+  _openVibePlyr() async {
+    var res = await _app.openMarket(appId: "io.mediaon.vibeplyr");
+    print(res);
   }
 }
